61bd7493db8f384f78b630f7577d8e1451caf56a
vinicius-vasconcelos <viniciussouzav@gmail.com>
2020-10-21 15:38:44 -0300

    criando connection

---
+++ models/connection.js
@@ -0,0 +1,24 @@
+const mongoClient = require('mongodb').MongoClient;
+
+//evaluator connection
+// const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
+// const DB_NAME = 'StoreManager';
+
+//local connection
+const MONGO_DB_URL = 'mongodb://localhost:27017/StoreManager';
+const DB_NAME = 'StoreManager';
+
+const connection = () => {
+  return mongoClient
+    .connect(MONGO_DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then((conn) => conn.db(DB_NAME))
+    .catch((err) => {
+      console.error(err);
+      process.exit(1);
+   });
+};
+
+module.exports = connection;
