059e67da932651bfa7e2ed78478020a7f3f3766e
vinicius-vasconcelos <viniciussouzav@gmail.com>
2020-10-25 15:59:12 -0300

    Criando model de sale

---
+++ models/Sale.js
@@ -0,0 +1,61 @@
+const conn = require('./connection');
+const { ObjectId } = require('mongodb');
+
+const add = async (itensSold) => {
+  try {
+    const db = await conn();
+    const result = await db.collection('sales').insertOne({ itensSold });
+
+    return result.ops[0];
+  } catch (error) {
+    return null;
+  }
+};
+
+const getAll = async () => {
+  try {
+    const db = await conn();
+    const data = await db.collection('sales').find({}).toArray();
+    return data;
+  } catch (error) {
+    return null;
+  }
+};
+
+const show = async (id) => {
+  try {
+    const db = await conn();
+    return await db.collection('sales').findOne(ObjectId(id));
+  } catch (error) {
+    return null;
+  }
+};
+
+const edit = async (id, itensSold) => {
+  try {
+    const db = await conn();
+    return await db.collection('sales').updateOne(
+      { _id: ObjectId(id) },
+      { $set: { itensSold }, },
+    );
+  } catch (error) {
+    return null;
+  }
+};
+
+const remove = async (id) => {
+  try {
+    const db = await conn();
+    return await db.collection('sales').deleteOne({ _id: ObjectId(id) });
+  } catch (error) {
+    return null;
+  }
+};
+
+module.exports = {
+  add,
+  getAll,
+  show,
+  edit,
+  remove,
+};
