959145d8b3571fddb2f462c19e8e61be743d55dc
vinicius-vasconcelos <viniciussouzav@gmail.com>
2020-10-24 22:32:41 -0300

    criando rota de produto

---
+++ routers/productRouter.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const middlewares = require('../middlewares/validation');
+const productController = require('../controllers/productController');
+
+const router = express.Router();
+
+router.post('/',
+  middlewares.isValidName,
+  middlewares.isValidQuantity,
+  productController.add
+);
+
+router.get('/', productController.getAll);
+
+router.get('/:id', productController.show);
+
+router.put('/:id',
+  middlewares.isValidName,
+  middlewares.isValidQuantity,
+  productController.edit
+);
+
+router.delete('/:id', productController.remove);
+
+module.exports = router;
