4c086e1092e872de142f416b465d577979609b9b
vinicius-vasconcelos <viniciussouzav@gmail.com>
2020-10-26 16:58:54 -0300

    fix c

--- controllers/saleController.js
+++ controllers/saleController.js
@@ -1,71 +1,53 @@
 const Sale = require('../models/Sale');
 
 const add = async (req, res) => {
-  // try {
-    const sale = await Sale.add(req.body);
-
-    return res.status(200).json(sale);
-  // } catch (error) {
-  //   return res.status(500).json({ err: { message: 'Fatal Error' } });
-  // }
+  try {
+    res.status(200).json(await Sale.add(req.body));
+  } catch (error) {
+    return res.status(500).json({ err: { message: 'Fatal Error' } });
+  }
 };
 
 const getAll = async (req, res) => {
-  // try {
-    const sales = await Sale.getAll();
-
-    return res.status(200).json({ sales });
-  // } catch (error) {
-  //   return res.status(500).json({ err: { message: 'Fatal Error' } });
-  // }
+  try {
+    res.status(200).json({ sales: await Sale.getAll() });
+  } catch (error) {
+    return res.status(500).json({ err: { message: 'Fatal Error' } });
+  }
 };
 
 const show = async (req, res) => {
-  // try {
+  try {
     const sale = await Sale.show(req.params.id);
 
     if (sale) return res.status(200).json(sale);
 
-    const err = {
-      code: 'not_found',
-      message: 'Sale not found',
-    };
-    return res.status(404).json({ err });
-  // } catch (error) {
-  //   return res.status(500).json({ err: { message: 'Fatal Error' } });
-  // }
+    res.status(404).json({ code: 'not_found', message: 'Sale not found', });
+  } catch (error) {
+    return res.status(500).json({ err: { message: 'Fatal Error' } });
+  }
 };
 
 const edit = async (req, res) => {
-  // try {
-    const { id } = req.params;
-
-    await Sale.edit(id, req.body);
-
-    return show(req, res);
-  // } catch (error) {
-  //   return res.status(500).json({ err: { message: 'Fatal Error' } });
-  // }
+  try {
+    await Sale.edit(req.params, req.body);
+    show(req, res);
+  } catch (error) {
+    return res.status(500).json({ err: { message: 'Fatal Error' } });
+  }
 };
 
 const remove = async (req, res) => {
-  // try {
+  try {
     const { id } = req.params;
-    const existsSale = await Sale.show(id);
 
-    if (!existsSale) {
-      const err = {
-        code: 'invalid_data',
-        message: 'Wrong sale ID format',
-      };
-      return res.status(422).json({ err });
-    } else {
-      await show(req, res);
-      return await Sale.remove(id);
-    }
-  // } catch (error) {
-  //   return res.status(500).json({ err: { message: 'Fatal Error' } });
-  // }
+    if (!await Sale.show(id)) return res.status(422).json({ code: 'invalid_data', message: 'Wrong sale ID format', });
+    
+    await show(req, res);
+    await Sale.remove(id);
+  } catch (error) {
+    return res.status(500).json({ err: { message: 'Fatal Error' } });
+  }
 };
 
 module.exports = {
